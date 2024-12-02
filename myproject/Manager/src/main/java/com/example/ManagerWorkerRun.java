304fd5c2759f0d0c7aa057213668167978930967
tamaricoh <106402042+tamaricoh@users.noreply.github.com>
2024-12-02 18:23:03 +0200

    Manager (without termination and closing)

--- myproject/Manager/src/main/java/com/example/ManagerWorkerRun.java
+++ myproject/Manager/src/main/java/com/example/ManagerWorkerRun.java
@@ -8,10 +8,10 @@ import java.nio.file.Paths;
 
 public class ManagerWorkerRun implements Runnable {
 
-    private static final String WORKERS_TO_MANAGER_QUEUE_NAME = "Workers-To-Manager";
+    private static final String WORKERS_TO_MANAGER_QUEUE_NAME = "Workers_To_Manager";
     private static final String MANAGER_TO_LOCALAPP_QUEUE_NAME = "Manager_To_LocalApp";
     // private static final String WORKERS_TO_MANAGER_BUCKET_NAME = "Workers-To-Manager";
-    private static final String MANAGER_TO_LOCALAPP_BUCKET_NAME = "Manager-To-LocalApp";
+    private static String CLIENT_BUCKET = "Text_File_Bucket";
 
     static AWSManeger aws = AWSManeger.getInstance();
     private Boolean terminate;
@@ -19,7 +19,7 @@ public class ManagerWorkerRun implements Runnable {
     private int numOfTasks;
     private int numOfCompletedTasks;
     private String localAppID;
-    private String[] messagesArray; // Array to store SQS messages
+    private String[] messagesArray; // Array to store messages
 
     public ManagerWorkerRun(int linesPerWorker, int numOfTasks, String localAppID) {
         terminate = false;
@@ -33,20 +33,20 @@ public class ManagerWorkerRun implements Runnable {
     @Override
     public void run() {
         while (!terminate && numOfCompletedTasks < numOfTasks) { // Stop when all tasks are processed
-            String[] msg = aws.getMessage(WORKERS_TO_MANAGER_QUEUE_NAME);
+            String[] msg = aws.getMessage(WORKERS_TO_MANAGER_QUEUE_NAME + "_" + localAppID);
             if (msg != null && msg.length == 3) { // Ensure message is valid
-                String sqsMsg = createSQSMessage(msg[0], msg[1], msg[2]);            
-                messagesArray[numOfCompletedTasks] = sqsMsg; // Store message in array
+                String stringMsg = msg[0] + " " + msg[1] + " " + msg[2];            
+                messagesArray[numOfCompletedTasks] = stringMsg; // Store message in array
                 numOfCompletedTasks++;
-                System.out.println("Message added to array: " + sqsMsg);
+                System.out.println("Message added to array: " + stringMsg);
             }
             if (numOfCompletedTasks == numOfTasks) {
                 String fileName = createSummaryFile();
                 if (fileName != null) {
-                    String s3Key = aws.uploadFileToS3(fileName, MANAGER_TO_LOCALAPP_BUCKET_NAME);
+                    String s3Key = aws.uploadFileToS3(fileName, CLIENT_BUCKET);
                     if (s3Key != null) {
                         System.out.println("File successfully uploaded to S3. S3 Key: " + s3Key);
-                        aws.sendSQSMessage("File successfully uploaded to S3. S3 Key: " + s3Key, MANAGER_TO_LOCALAPP_QUEUE_NAME);
+                        aws.sendSQSMessage(localAppID + " " + s3Key, MANAGER_TO_LOCALAPP_QUEUE_NAME);
                     } else {
                         System.err.println("Error uploading file to S3.");
                     }
@@ -56,9 +56,9 @@ public class ManagerWorkerRun implements Runnable {
         System.out.println("All tasks processed. Total messages: " + numOfCompletedTasks);
     }
 
-    private String createSQSMessage(String operation, String url, String newURL) {
-        return "Operation: " + operation + " old URL: " + url + " new URL: " + newURL;
-    }
+    // private String createSQSMessage(String operation, String url, String newURL) {
+    //     return  operation +  url  + newURL;
+    // }
 
     private String createSummaryFile() {
         String fileName = "summary_" + localAppID + ".txt";
